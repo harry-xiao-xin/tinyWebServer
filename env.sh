632361d16d0cfe96603b3a55d8711fcd419f1769
kevin-xiao-xin <444047485@qq.com>
2024-06-09 10:38:46 +0800

    update env config

--- env.sh
+++ env.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+apt install -y mysql-server mysql-client libmysqlclient-dev
 wget https://tangentsoft.com/mysqlpp/releases/mysql++-3.2.5.tar.gz
 tar -zxvf ./mysql++-3.2.5.tar.gz
 cd  mysql++-3.2.5
  ./configure
-make
+make -j8
 make install
